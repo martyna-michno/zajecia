48621255e14be6288f68e246ea9495bb01a69a19
Martyna Michno <mjjmichno@gmail.com>
2019-10-14 22:15:46 +0200

    zad 5 i 6

--- zadania zajecia js/script.js
+++ zadania zajecia js/script.js
@@ -1,1 +1,7 @@
 document.querySelector('p').innerText = 'nowy tekst blablabla';
+
+let powitanie = 'dzien dobry';
+const podatek = 0.19;
+
+let paragraf1 = document.querySelectorAll('p')[0].innerText;
+
