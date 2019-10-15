4321d0fa38f3f3cc6e3a022138ce81b01015140e
Martyna Michno <mjjmichno@gmail.com>
2019-10-15 13:59:57 +0200

    up

--- zadania zajecia js/script.js
+++ zadania zajecia js/script.js
@@ -5,3 +5,9 @@ const podatek = 0.19;
 
 let paragraf1 = document.querySelectorAll('p')[0].innerText;
 
+let liczba = 10;
+let zdanie = "to jest zdanie";
+let wartlogiczna = true;
+
+console.log(typeof(zdanie));
+
