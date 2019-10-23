47bc4c90a55be826fa30847bba99377bf9ac2ecd
Martyna Michno <mjjmichno@gmail.com>
2019-10-23 09:51:29 +0200

    added two students(objects)

--- programowanie obiektowe/index.js
+++ programowanie obiektowe/index.js
@@ -1,1 +1,40 @@
 console.log('Hello World!')
+
+
+
+
+var Student1 = {
+    name: "Martyna",
+    adress: "Sloneczna 12/4",
+    studentIdNumber: 281038,
+    profile: "Material Engineering",
+    DisplayFullInfo: function () {
+
+        console.log(this.name, this.adress, this.studentIdNumber, this.profile);
+
+    },
+    ChangeId: function (argument) {
+
+        this.studentIdNumber = argument;
+
+    }
+
+}
+
+var Student2 = {
+    name: "Jagoda",
+    adress: "Wielkokacka 13/4",
+    studentIdNumber: 950707,
+    profile: "Law",
+    DisplayFullInfo: function () {
+
+        console.log(this.name, this.adress, this.studentIdNumber, this.profile);
+
+    },
+    ChangeId: function (argument) {
+
+        this.studentIdNumber = argument;
+
+    }
+
+}
