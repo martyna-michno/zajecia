30ec11107e0e262369062ed02d139c064a935d89
Martyna Michno <mjjmichno@gmail.com>
2019-10-23 11:27:37 +0200

    studentslist created and filled with students objects

--- programowanie obiektowe/index.js
+++ programowanie obiektowe/index.js
@@ -38,29 +38,62 @@ var student2_proba = {
 }
 
 
+// var student = {
+//     name: '',
+//     setName: function (name) {
 
-var studentslist = [];
+//         this.name = name
+//     },
+//     adress: '',
+//     setAdress: function (adress) {
+
+//         this.adress = adress;
+//     },
+//     studentIdNumber: '',
+//     setId: function (IdNumber) {
+
+//         this.studentIdNumber = IdNumber.toString();
+
+//     },
+//     profile: '',
+//     setProfile: function (profile) {
+
+//         this.profile = profile;
+
+//     },
+//     displayFullInfo: function () {
+
+
+//         console.log(`Imię: ${this.name} Adres: ${this.adress} NumerId: ${this.studentIdNumber} Profil: ${this.profile}`);
+
+//     },
+//     ChangeId: function (newId) {
+
+//         this.studentIdNumber = newId.toString();
+
+//     }
+
+// }
+
+// var student1 = Object.create(student);
+// student1.setName("Jan");
+// student1.setAdress("Gdansk");
+// student1.setId(123);
+// student1.setProfile("Medical student");
+
+
+var studentsList = [];
 
 var student = {
     name: '',
-    setName = function (name) {
-
-        this.name = name
-    },
     adress: '',
-    setAdress = function (adress) {
-
-        this.adress = adress;
-    },
     studentIdNumber: '',
-    setId = function (IdNumber) {
-
-        this.studentIdNumber = IdNumber.toString();
-
-    },
     profile: '',
-    setProfile = function (profile) {
+    setData: function (name, adress, IdNumber, profile) {
 
+        this.name = name;
+        this.adress = adress;
+        this.studentIdNumber = IdNumber;
         this.profile = profile;
 
     },
@@ -79,4 +112,15 @@ var student = {
 }
 
 var student1 = Object.create(student);
-student1.setName("Jan").setAdress("Gdansk").setId(123).setProfile("Medical student");
+student1.setData("Marek", "Gdańsk", "2930", "Law");
+
+var student2 = Object.create(student);
+student2.setData("Ania", "Gdynia", "4590", "Medicine");
+
+var student3 = Object.create(student);
+student3.setData("Joanna", "Warszawa", "3499", "Businnes");
+
+var student4 = Object.create(student);
+student4.setData("Martyna", "Sopot", "1240", "Sport");
+
+studentsList.push(student1, student2, student3, student4);
