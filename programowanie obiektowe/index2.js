cf7ac851d65f390f96e075aa5fdc922ad9ce7f5d
Martyna Michno <mjjmichno@gmail.com>
2019-10-24 10:51:11 +0200

    white list added to functionality

--- programowanie obiektowe/index2.js
+++ programowanie obiektowe/index2.js
@@ -10,6 +10,7 @@ function PersonDB() {
   PersonDB.prototype.removePerson = function(removeIndex) {
     this.database.splice(removeIndex, 1);
     renderInstance.setHtml(dbInstance.database);
+    renderInstance1.setHtml(dbInstance1.database);
   };
   
   PersonDB.prototype.displayPeople = function() {
@@ -27,24 +28,21 @@ function PersonDB() {
   
   
   // Class for render methods
-  function Render(container, button, inputName, inputSurname, inputAge, inputRole) {
+  function Render(container, button, inputName, inputSurname, inputAge, inputRole, db, dbName) {
     this.container = document.getElementById(container);
     this.button = document.getElementById(button);
     this.inputName = document.getElementById(inputName);
     this.inputSurname = document.getElementById(inputSurname);
     this.inputAge = document.getElementById(inputAge);
     this.inputRole = document.getElementById(inputRole);
-  };
-
-
-
-  Render.prototype.renderRow = function(name, surname, age, role, index) {
-    return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div><button onclick="dbInstance.removePerson('+index+')">Jednak lubię tą osobę</button></div>';
+    this.db = db;
+    this.dbName = dbName;
   };
   
-
-
-
+  Render.prototype.renderRow = function(name, surname, age, role, index) {
+    return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div><button onclick="'+this.dbName+'.removePerson('+index+')">Usuń z tej listy</button></div>';
+  };
+  
   Render.prototype.setHtml = function(database) {
     this.container.innerHTML = '';
     var self = this;
@@ -53,7 +51,6 @@ function PersonDB() {
     });
   };
   
-
   Render.prototype.getValuesFromInput = function() {
     return {
       name: this.inputName.value,
@@ -62,25 +59,30 @@ function PersonDB() {
       role: this.inputRole.value,
     }
   };
-
-
+  
   Render.prototype.addClick = function() {
     var self = this;
     this.button.addEventListener('click', function(){
       var person = self.getValuesFromInput();
   
-      dbInstance.addPerson(person);
-      renderInstance.setHtml(dbInstance.database);
+      self.db.addPerson(person);
+      self.setHtml(self.db.database);
     });
   };
   
-
+  
+  
   // Create object of PersonDB
   var dbInstance = new PersonDB();
+  var dbInstance1 = new PersonDB();
   
   // Create object of Render
-  var renderInstance = new Render('records', 'addButton', 'inputName', 'inputSurname', 'inputAge', 'inputRole');
+  var renderInstance = new Render('records', 'addButton', 'inputName', 'inputSurname', 'inputAge', 'inputRole' , dbInstance, 'dbInstance');
+  var renderInstance1 = new Render('records1', 'addButton1', 'inputName1', 'inputSurname1', 'inputAge1', 'inputRole1', dbInstance1, 'dbInstance1');
+
+
   renderInstance.addClick();
   renderInstance.setHtml(dbInstance.database);
 
-
+  renderInstance1.addClick();
+  renderInstance1.setHtml(dbInstance1.database);
