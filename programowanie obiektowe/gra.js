e91bcb629ae3a2ebe9d027bc591542311f8deb8e
Martyna Michno <mjjmichno@gmail.com>
2019-10-25 15:30:13 +0200

    two solutions working

--- programowanie obiektowe/gra.js
+++ programowanie obiektowe/gra.js
@@ -1,32 +1,32 @@
-// let container = document.querySelector('.container');
-// let player = document.querySelector('.counter');
+let container = document.querySelector('.container');
+let player = document.querySelector('.counter');
 
-// //key = arrowleft, keycode = numerek
+//key = arrowleft, keycode = numerek
 
-// document.onkeydown = function (e) {
+document.onkeydown = function (e) {
 
-//     let actualPositionX = player.offsetLeft;
-//     let actualPositionY = player.offsetTop;
+    let actualPositionX = player.offsetLeft;
+    let actualPositionY = player.offsetTop;
 
-//     switch (e.key) {
-//         case 'ArrowLeft':
-//             console.log('lewo');
-//             return player.style.left = `${(actualPositionX - 5).toString()}px`;
-//             break;
-//         case 'ArrowRight':
-//             console.log('prawo');
-//             return player.style.left = `${(actualPositionX + 5).toString()}px`;
-//             break;
-//         case 'ArrowUp':
-//             console.log('góra');
-//             return player.style.top = `${(actualPositionY - 5).toString()}px`;
-//             break;
-//         case 'ArrowDown':
-//             console.log('dół');
-//             return player.style.top = `${(actualPositionY + 5).toString()}px`;
-//             break;
-//     }
-// };
+    switch (e.key) {
+        case 'ArrowLeft':
+            console.log('lewo');
+            return player.style.left = `${(actualPositionX - 5).toString()}px`;
+            break;
+        case 'ArrowRight':
+            console.log('prawo');
+            return player.style.left = `${(actualPositionX + 5).toString()}px`;
+            break;
+        case 'ArrowUp':
+            console.log('góra');
+            return player.style.top = `${(actualPositionY - 5).toString()}px`;
+            break;
+        case 'ArrowDown':
+            console.log('dół');
+            return player.style.top = `${(actualPositionY + 5).toString()}px`;
+            break;
+    }
+};
 
 
 // function checkBorders() {
